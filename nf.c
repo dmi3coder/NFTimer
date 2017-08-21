da4e98cadbad1d40a82a888da4d4dbcbb5c286f5
dmi3coder <dmi3coder@gmail.com>
2017-08-21 22:57:22 +0300

    Showing difference in seconds and days

--- nf.c
+++ nf.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include "TimeHandler.h"
 #define TARGET_TIME 1509994820
-
+#define SECONDS_PER_DAY 86400
 
 void printDefaultInfo(){
 	printf("Current time: %s", getTimeInString(0));
@@ -11,14 +11,35 @@ void printDefaultInfo(){
 	printf("Seconds left: %ld\n", TARGET_TIME-time(NULL));
 }
 
+void handleArg(char arg);
+
 void handleArgs(int argc, char** argv) {
 	switch(argc){
 		case 1:
 			printDefaultInfo();
 			break;
+		case 2:
+			if(argv[1][0] == '-'){
+				handleArg(argv[1][1]);
+			}
+			break;
+
 		default:
 			printf("NFTimer don't know how to handle this yet\n");
 
+	}
+}
+
+void handleArg(char arg) {
+	switch(arg){
+		case 'd':
+			printf("Difference: %ld\n", TARGET_TIME-time(NULL));
+			break;
+		case 'D':
+			printf("Difference in days %ld\n", (TARGET_TIME-time(NULL))/SECONDS_PER_DAY);
+			break;
+		default:
+			printf("No such command\n");
 	}
 }
 
